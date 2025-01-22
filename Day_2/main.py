4746c048b2a1fa9629a9c2d1c8508add89af5f43
rokin-emergys <rajeev.aken@emergys.com>
2025-01-22 15:56:16 +0530

    minor fix

--- Day_2/main.py
+++ Day_2/main.py
@@ -11,11 +11,8 @@ num1 = 10
 num2 = 5
 
 print(add(num1, num2))
-
 print(subtract(num1, num2))
-
 print(multiply(num1, num2))
-
 print(divide(num1, num2))
 
 # Task 2. Remove duplicates from a list of data.
