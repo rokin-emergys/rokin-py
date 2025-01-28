2a99ac5200a1a02ae7ebe9ccb6d57b40c75c8077
rokin-emergys <rajeev.aken@emergys.com>
2025-01-28 16:06:15 +0530

    Day-4 added

--- Day_2/assignment_proj/main.py
+++ Day_2/assignment_proj/main.py
@@ -2,7 +2,7 @@ from file_processing.file_manager import *
 from user_management.user_handler import *
 from utils.string_tools import *
  
- 
+summary = [] 
 def string_ops(choice):
     """Function to perform string operations
     Args:
@@ -16,24 +16,28 @@ def string_ops(choice):
                 user1=input(" Enter the String : ")
                 try:
                     print(f" The length of string is {word_count(user1)}")
+                    summary.append(f" The length of string is {word_count(user1)}")
                 except Exception as e:
                     print(e)
             case 2:
                 try:
                     user1=input(" Enter the String : ")
                     print(f" The reverse of the string is {reverse_words(user1)}")
+                    summary.append(f" The reverse of the string is {reverse_words(user1)}")
                 except Exception as e:
                     print(e)
             case 3:
                 try:
                     user1=input(" Enter the String : ")
                     print(f" The string is palindrome : {check_palindrome(user1)}")
+                    summary.append(f" The string is palindrome : {check_palindrome(user1)}")
                 except Exception as e:
                     print(e)
             case 4:
                 try:
                     user1=input(" Enter the String : ")
                     print(f" The most common word is {find_most_common_word(user1)}")
+                    summary.append(f" The most common word is {find_most_common_word(user1)}")
                 except Exception as e:
                     print(e)
             case _:
@@ -94,53 +98,85 @@ def user_management(users,choice):
     return users
  
  
- 
 def main_menu():
-    """Function to display the main menu
-    Args:
-        None
-    Returns:
-        None
-    """
     users=[]
     while True:
         try:
-            print("****************************** Main Menu ******************************")
+            print("\n")
+            print("****************************** Main Menu *********************************************")
             print("1. Text Analysis")
             print("2. User Management")
             print("3. File Processing")
             print("4. Exit")
- 
+            print("**************************************************************************************")
+            print("\n")
             choice = int(input("Enter your choice: "))
             match choice:
                 case 1:
+                    print("\n")
                     print("******************************Welcome to the text analysis ******************************")
                     print(" Enter 1 to get word count \n Enter 2 to reverse input \n Enter 3 to palindrome \n Enter 4 to find most common words \n Enter 5 to exit")
+                    print("*****************************************************************************************")
+                    print("\n")
                     try:
                         input_one=int(input("Enter the String choice : "))
                         string_ops(input_one)
                     except ValueError:
                         print("Invalid choice\n")
                 case 2:
+                    print("\n")
                     print("******************************Welcome to the user management system******************************")
                     print(" Enter 1 to add user \n Enter 2 to update user \n Enter 3 to get user info \n Enter 4 to remove user \n Enter 5 to exit")
+                    print("**************************************************************************************")
+                    print("\n")
                     try:
                         input1=int(input("Enter your choice : "))
                         user_management(users,input1)
-                        if input1==5:
-                            break
                     except ValueError:
                         print("Invalid choice\n")
                     except Exception as e:
                         print(e)
                 case 3:
-                    # Save the users list to a file named users_data.txt.
-                    save_users_to_file("users_data.txt",users)
+                    print("\n")
+                    print("******************************Welcome to the file processing system******************************")
+                    print(" Enter 1 to load users from file \n Enter 2 to save users to file \n Enter 3 to write summary \n Enter 4 to exit")
+                    print("*************************************************************************************************")
+                    print("\n")                   
+                    try:
+                        while True:
+                            input2=int(input("Enter your choice : "))
+                            match input2:
+                                case 1:
+                                    try:
+                                        users=load_users_from_file("users_data.txt")
+                                        print("Users loaded successfully")
+                                    except Exception as e:  
+                                        print(e)
+                                case 2:
+                                    try:
+                                        save_users_to_file("users_data.txt",users)
+                                        print("Users saved successfully")
+                                    except Exception as e:
+                                        print(e)
+
+                                case 3:
+                                    try:
+                                        summery_text="\n".join(map(str,summary))
+                                        write_summary("summary_of_data.txt",summery_text)
+                                        print("Summary written successfully")
+                                    except Exception as e: 
+                                        print(e)
+                                case 4:
+                                    break
+                    except ValueError:
+                        print("Invalid choice\n")
+                    except Exception as e:
+                        print(e)
                     pass
                 case 4:
                     break
         except ValueError:
-            print("Invalid choice\n")
+            print("Invalid choice\n") 
 
 
 if __name__ == "__main__":
