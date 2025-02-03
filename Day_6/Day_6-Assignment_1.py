f335c003efad3983994338a31e0308047d744a32
rokin-emergys <rajeev.aken@emergys.com>
2025-02-03 11:14:26 +0530

    minor fix & Day-6 Assignment added

---
+++ Day_6/Day_6-Assignment_1.py
@@ -0,0 +1,35 @@
+import json
+import asyncio
+import timeit 
+import aiohttp
+
+def log_execution_time(func):
+    async def wrapper(*args,**kargs):
+        start=timeit.default_timer()
+        print(f"started feching {args[0]}")
+        result=await func(*args,**kargs)
+        total=timeit.default_timer()-start
+        print(f"completed fetching {args[0]} it took {total} sec")
+        return result
+    return wrapper
+
+
+@log_execution_time
+async def fetch_url(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.json()
+
+
+async def main():
+    urls=[ "https://jsonplaceholder.typicode.com/users/6",
+        "https://jsonplaceholder.typicode.com/users/4",
+        "https://jsonplaceholder.typicode.com/todos/19" ]
+    
+    results=await asyncio.gather(*(fetch_url(url) for url in urls ))
+
+    for url,result in zip(urls,results):
+        print(f"Response from {url} is {json.dumps(result, indent=4)}\n")
+
+if __name__=="__main__" :
+    asyncio.run(main())
